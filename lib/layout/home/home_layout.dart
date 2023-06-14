035446af66bda9dca7e795203151ddb5093afa31
pc <ShehabEl-dinSaid13>
2023-06-14 02:14:06 +0200

    initial project

---
+++ lib/layout/home/home_layout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeLayout extends StatelessWidget {
+  static String routeName = 'Home_layout';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color:Colors.green ,),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/layout/home/home_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+      ),
+      routes: {
+        HomeLayout.routeName:(context) => HomeLayout()
+      },
+      initialRoute:HomeLayout.routeName ,
+
+    );
+  }
+}
+
